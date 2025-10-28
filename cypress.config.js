f784890c98f0573048d9c36f91e2bbbce09a3a15
Krzysztof Kołodziński <krzysztof.kolodzinski@problematy.pl>
2025-10-28 19:20:36 +0100

    some works

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
+const http = require("http");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000/',
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
+      // Task to fetch from webpack-dev-server
+      on('task', {
+        fetchWebpackScript() {
+          return new Promise((resolve, reject) => {
+            http.get('http://localhost:8080/index.js', (res) => {
+              let data = '';
+              res.on('data', chunk => data += chunk);
+              res.on('end', () => resolve(data));
+            }).on('error', (err) => reject(err));
+          });
+        }
+      });
       return config;
     },
     experimentalStudio: true,
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,28 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// Workaround for webpack-dev-server ERR_EMPTY_RESPONSE in Cypress
+// Fetch script from webpack-dev-server using Node.js task and intercept browser requests
+let scriptContent = null;
+
+before(() => {
+  // Fetch the script content once using Node.js
+  cy.task('fetchWebpackScript').then((content) => {
+    scriptContent = content;
+  });
+});
+
+beforeEach(() => {
+  // Intercept requests and serve the cached content
+  cy.intercept('GET', 'http://localhost:8080/index.js', (req) => {
+    req.reply({
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/javascript; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: scriptContent,
+    });
+  });
+});
